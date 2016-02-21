36f80a1b6af3a8d8d8fa28b8c86933f0e2704670
jonroth <jonroth@users.noreply.github.com>
2016-02-21 15:50:26 -0500

    Have working simulation code where means are different depending on J_tminus1. Appears to be working okay

--- Simulate_Data/calculate_offerings.m
+++ Simulate_Data/calculate_offerings.m
@@ -9,7 +9,6 @@ global lambda;
 global theta_c;
 global theta_g;
 
-global gamma;
 
 
 %For now ,we assume that sigma_zetaj = sigma_nu, sigma_zetajft =
@@ -19,28 +18,40 @@ sigma_zetajft = sigma_epsilon;
 
 %Calculate the Nu draws and Epsilon draws using the shocks and then given
 %means and covariances
-Nu_draws_array =  Mu_f_array + gamma* theta_g * G_array + sigma_nu * Eta_shocks_array;
-Epsilon_draws_array = sigma_epsilon * Epsilon_shocks_array;
 
-Pi_star_array = Nu_draws_array + Epsilon_draws_array;
+%Eta_draws_array =  Mu_f_array + gamma* theta_g * G_array + sigma_nu * Eta_shocks_array;
+%Epsilon_draws_array = sigma_epsilon * Epsilon_shocks_array;
+%Pi_star_array = Eta_draws_array + Epsilon_draws_array;
 
 
 J_tminus1_array = NaN(F,J,T);
 J_tminus1_array(:,:,1) = J_0;
 
 J_t_array = NaN(F,J,T);
-
+Pi_star_array = NaN(F,J,T);
 
 for t=1:T
    
     J_tminus1 = J_tminus1_array(:,:,t);
-    Pi_star_t = Pi_star_array(:,:,t);
+    
+    
+    
+    %Compute the eta and epsilon shocks for period t by shifting and
+    %scaling the standard normal shocks by the appropriate means and
+    %variances
+    %Right now, eta has mean mu_f + theta_g g if j is not in J_tminus1; eta
+    %has mean mu_f +(lambda * theta_g g) if j is in J_tminus1
+    Eta_t = Mu_f_array(:,:,t) + ( J_tminus1 * lambda + (1 - J_tminus1) ) .* (  theta_g * G_array(:, :, t) ) + sigma_nu * Eta_shocks_array(:,:,t) ;
+    Epsilon_t = sigma_epsilon * Epsilon_shocks_array(:,:,t);
+    
+    Pi_star_t = Eta_t + Epsilon_t;
+    Pi_star_array(:,:,t) = Pi_star_t;
     
     %Calculate profit (including FCs) from adding J in period t, using fixed costs
     %dependent on J_tminus1
     Pi_t = Pi_star_t - (J_tminus1 .* lambda + (1 - J_tminus1) ) .* (theta_c + theta_g * G_array(:, :, t) );
     
-    %j is in J_t if profit is greater than 0
+    %j is in J_t if profit including fixedcosts is greater than 0
     J_t = (Pi_t >= 0);
     J_t_array(:,:,t) = J_t;
     
@@ -51,6 +62,7 @@ for t=1:T
 end
 
 
+%Add shocks to pi_star to generate pi
 Pi_array = Pi_star_array .* (1 + sigma_zetaj * Zetaj_shocks_array + sigma_zetajft * Zetajft_shocks_array);
 
 
--- Simulate_Data/simulate_data_main.m
+++ Simulate_Data/simulate_data_main.m
@@ -5,15 +5,14 @@ global theta_c;
 theta_c = 129.73;
 
 global lambda;
-%lambda = 0.386; %should this be negative? Should this be one-over this?
-lambda = 1;
+lambda = 0.386; %should this be negative? Should this be one-over this?
 
 global theta_g;
 theta_g = 21.38;
 
 
 global burnout;
-burnout = 500;
+burnout = 1000;
 
 
 global T;
@@ -26,9 +25,6 @@ global F;
 F = 9;
 
 
-global gamma;
-%gamma = .75; 
-gamma =1 ;
 f_names = {'Chrysler'; 'Ford' ;'Daimler'; 'GM'; 'Hino'; 'International'; 'Isuzu'; ...
     'Paccar'; 'Volvo'} ;
 
@@ -37,7 +33,7 @@ f_share = [5.56; 12.5; 12.5+4.17 + 1.39; 6.94; 4.17; 16.67; 6.94; 6.94+11.11; 2.
 
 
 
-mu_f_vec = repmat(50,F,1);
+mu_f_vec = repmat(100,F,1);
 
 %% Make covariate arrays
 
@@ -103,14 +99,14 @@ Mu_f_array = repmat(mu_f_vec, 1,J,T);
 
 
 %% Draw data
-sigma_nu = 100;
-sigma_epsilon = 25;
+sigma_nu = 50;
+sigma_epsilon = 50;
 
 
-J_t_startat1 = calculate_offerings( sigma_nu, ...
+[J_t_startat1, J_tm1_startat1] = calculate_offerings( sigma_nu, ...
     sigma_epsilon, ones(F,J) , Epsilon_shocks_array, Eta_shocks_array, Zetaj_shocks_array,Zetajft_shocks_array, Mu_f_array, G_array, F_array );
 
-J_t_startat0 = calculate_offerings( sigma_nu, ...
+[J_t_startat0, J_tm1_startat0] = calculate_offerings( sigma_nu, ...
     sigma_epsilon, zeros(F,J) , Epsilon_shocks_array, Eta_shocks_array, Zetaj_shocks_array,Zetajft_shocks_array, Mu_f_array, G_array, F_array );
 
 
@@ -123,11 +119,20 @@ meanyrt = @(J,t) mean( mean(J(:,:,t)) );
 meanyr_J0 = @(t) meanyrt(J_t_startat0, t);
 meanyr_J1 = @(t) meanyrt(J_t_startat1, t);
 
+meanyr_J0_tm1 = @(t) meanyrt(J_tm1_startat0, t);
+meanyr_J1_tm1 = @(t) meanyrt(J_tm1_startat1, t);
+
+
 J0_means = arrayfun( meanyr_J0 , (1:T)' );
 J1_means = arrayfun( meanyr_J1 , (1:T)' );
 
+J0_means_tm1 = arrayfun( meanyr_J0_tm1 , (1:T)' );
+J1_means_tm1 = arrayfun( meanyr_J1_tm1 , (1:T)' );
+
+
 plot( [(1:T)', (1:T)'], [J0_means, J1_means] )
 
+[J0_means_tm1, J1_means_tm1]
 %plot( [(1:T)'], [J0_means] )
 
 
