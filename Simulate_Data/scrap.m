6e7bed413b1195af12d61f35f901242773a87806
jonroth <jonroth@users.noreply.github.com>
2016-04-04 22:54:53 -0400

    Have a loop to calculate the moments for each of the datasets and tally whether they are satisfied at the true params. Seems to be working although all or almost all hold

--- Simulate_Data/scrap.m
+++ Simulate_Data/scrap.m
@@ -1,19 +1,6 @@
+ds =1;
+load( strcat('../../Output/Simulated_Data/ds', num2str(ds), '.mat' ));
 
-load('../../Output/Simulated_Data/calibrated_params');
+C_moment1 = (J_t_vec == 1) & (J_tminus1_vec == 1);
 
- [J_t_array, J_tminus1_array, Pi_star_array] = calculate_offerings( sigma_nu, ...
-        sigma_eps, zeros(F,J) , Epsilon_shocks_array, Eta_shocks_array, mu_f, G_array );
-    
- 
- Jt1 =  J_t_array(:,:,1);
- Jt0 =  J_tminus1_array(:,:,1);
- 
- Pistar1 = Pi_star_array(:,:,1);
- 
- testJ = (Pi_star_array -  (J_tminus1_array * lambda + (1 - J_tminus1_array))...
-     .* (theta_c + G_array *theta_g)      ) >= 0 ;
- 
- diffJ = J_t_array - testJ;
- 
- sum( abs( diffJ(:) ) )
- 
+sum( Pi_vec .* C_moment1) ./ sum( C_moment1) - lambda * ( theta_c + theta_g * mean(g_vec) )
