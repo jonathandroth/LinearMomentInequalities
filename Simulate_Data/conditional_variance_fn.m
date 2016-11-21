7fd0cdf0585bbedc6e2dcd699df72f2838fe7808
jonathandroth <jonathanroth@g.harvard.edu>
2016-11-21 17:11:21 -0500

    Updated the conditional variance function to remove linearly dependent rows

--- Simulate_Data/conditional_variance_fn.m
+++ Simulate_Data/conditional_variance_fn.m
@@ -12,9 +12,13 @@ function conditional_variance = conditional_variance_fn(y, X, diagonol)
         Sigma_X = diag( diag( Sigma_X) );
         %display('Using Diagonal Matrix');
     
+    %If Sigma_X does not have full rank, take an independent subset of the
+    %columns of X
     elseif( rank(Sigma_X) < size(Sigma_X,1) )
+        [~,licols] = rref( cov(Sigma_X) );
         
-        Sigma_X = Sigma_X + 0.01 * diag( diag( Sigma_X) );
+        X = X(:, licols);
+        Sigma_X = Sigma_X(licols, licols);
     end
     
     y_matched = y( closest_neighbor_indices(  X , Sigma_X), :) ;
