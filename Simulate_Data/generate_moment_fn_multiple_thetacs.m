1f372ef364ffdbe803ce6f9f0d30f2b2b9b20804
jonathandroth <jonathanroth@g.harvard.edu>
2016-12-01 20:55:46 -0500

    Multiple thetas is running

--- Simulate_Data/generate_moment_fn_multiple_thetacs.m
+++ Simulate_Data/generate_moment_fn_multiple_thetacs.m
@@ -1,4 +1,4 @@
-function A_g_cell = generate_moment_fn_multiple_thetacs( F_group_cell, F_array, G_array, Eta_jt_shocks_array, Eta_t_vec, Pi_array, J_t_array, J_tminus1_array)
+function [A_g_cell, A_c_cell, Y_cell] = generate_moment_fn_multiple_thetacs( F_group_cell, F_array, G_array, Eta_jt_shocks_array, Eta_t_vec, Pi_array, J_t_array, J_tminus1_array)
 
 total_num_firms = size(F_array,1);     %Use the fact that rows index firms, so that the first 
 num_f_groups = size(F_group_cell,1);
@@ -6,6 +6,8 @@ num_f_groups = size(F_group_cell,1);
 
 
 A_g_cell = cell(num_f_groups,1);
+A_c_cell = cell(num_f_groups,1);
+Y_cell = cell(num_f_groups,1);
 
 for(f_group_index = 1:num_f_groups)
 
@@ -22,6 +24,9 @@ for(f_group_index = 1:num_f_groups)
         J_t_array(subset_indicator, : ,:), J_tminus1_array(subset_indicator,:,:)); 
 
     A_g_cell{f_group_index, 1} = A_g;
+    A_c_cell{f_group_index, 1} = A_c;
+    Y_cell{f_group_index, 1} = Y;
+    
     
     
 end
