0ac686b78eaf40b5698ea08395d1cb41de4aeea6
jonathandroth <jonathanroth@g.harvard.edu>
2017-03-09 21:12:46 -0500

    Modified the set parameters script to automatically detect whether on laptop or server and set directories and numdatasets accordingly. Also added a script to run the lp inference for lambda with 3 parameters

--- Simulate_Data/lp_lambda_inference_script.m
+++ Simulate_Data/lp_lambda_inference_script.m
@@ -1,16 +1,3 @@
-%Specify where the output should go (can be relative to the working  dir)
-data_output_dir = '../../Output/Conditional_FullMatrix/Data/Single_Thetac/';
-figures_output_dir = '../../Figures/Conditional_FullMatrix/LP_figures/Single_Thetac/';
-
-filename_graph = 'lambda_rejection_probabilities';
-%Specify the groups of firms that have different coefficients
-F_group_cell = {[1;2;3;4;5;6;7;8;9]};
-
-
-%num_F_groups = size(F_group_cell,1);
-%l = [1; zeros(num_F_groups-1,1); mean_g];
-
-use_basic_moments = 0;
 %% Specify parameters
 lp_set_parameters
 %% Import data and calculate moments and covariance matrices
