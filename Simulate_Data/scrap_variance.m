0de4ad14cee0e9621ec15432673f46c72950ca4d
jonathandroth <jonathanroth@g.harvard.edu>
2017-05-09 14:49:57 -0400

    Made minor change to the differences plots, and added a scrap file playing with the conditonal and unconditioanl variance estiamtes

--- Simulate_Data/scrap_variance.m
+++ Simulate_Data/scrap_variance.m
@@ -1,7 +1,8 @@
 
 
 cov_conditional = conditional_variance_fn(Y_basic, A_basic, diagonal);
-cov_unconditional = cov(Y_basic + A_c_basic * theta_c_true + A_g_basic * theta_g_true ) 
+%cov_unconditional = cov(Y_basic + A_c_basic * theta_c_true + A_g_basic * theta_g_true ) ;
+cov_unconditional = cov(Y_basic + A_c_basic * (theta_c_true) + A_g_basic * (theta_g_true+20) ) ;
 %cov_unconditional = cov(Y_basic ) 
 
 
@@ -9,5 +10,18 @@ cov_conditional
 cov_unconditional
 
 [~,p] = chol( cov_unconditional - cov_conditional )
-
 eig(cov_unconditional - cov_conditional )
+
+Z_draws = randn( size(cov_conditional,1) , 10000);
+
+cutoff_conditional = c_lf(cov_conditional, alpha, Z_draws)
+cutoff_unconditional = c_lf(cov_unconditional, alpha, Z_draws)
+
+[cutoff_conditional * diag(cov_conditional),...
+cutoff_unconditional * diag(cov_unconditional)]
+
+g_T = -sum(Y_basic + A_c_basic * (theta_c_true) + A_g_basic * (theta_g_true+20))' / sqrt( size(Y_basic,1) );
+
+basic_tests(g_T, cov_conditional, Z_draws, alpha, beta)
+
+basic_tests(g_T, cov_unconditional, Z_draws, alpha, beta)
