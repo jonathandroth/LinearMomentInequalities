8bf2b22393773cbe2ba6793f82b06e25ac9f5e84
jonathandroth <jonathanroth@g.harvard.edu>
2017-06-12 19:14:15 -0400

    Updated run scripts

--- Simulate_Data/run_lp_graphs_only.m
+++ Simulate_Data/run_lp_graphs_only.m
@@ -1,7 +1,7 @@
 
 
 graphs_only = 1
-remote_to_server = 1
+remote_to_server = 0
 
 xsplit_graph_meanweight = [10,80]
 xlim_graph_meanweight = [-10,100]
@@ -9,5 +9,5 @@ xlim_graph_meanweight = [-10,100]
 xsplit_graph_thetag = [-70,30]
 xlim_graph_thetag = [-90,50]
 
-
-run_lp_cs_3thetacs_basicmoments
+run_lp_cs_1thetac_basicmoments
+%run_lp_cs_3thetacs_basicmoments
