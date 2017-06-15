d3ea4f5eb6d5fe22b38ed8d388afe74825ea4b76
jonathandroth <jonathanroth@g.harvard.edu>
2017-06-15 16:04:35 -0400

    Have script that makes pretty decent graphs with the axis split for thetag and mean weight

--- Simulate_Data/run_lp_graphs_only.m
+++ Simulate_Data/run_lp_graphs_only.m
@@ -1,7 +1,7 @@
 
 
 graphs_only = 1
-remote_to_server = 0
+remote_to_server = 1
 
 xsplit_graph_meanweight = [10,80]
 xlim_graph_meanweight = [-10,100]
@@ -10,4 +10,14 @@ xsplit_graph_thetag = [-70,30]
 xlim_graph_thetag = [-90,50]
 
 run_lp_cs_1thetac_basicmoments
-%run_lp_cs_3thetacs_basicmoments
+run_lp_cs_3thetacs_basicmoments
+run_lp_cs_9thetacs_basicmoments
+
+
+xsplit_graph_meanweight = [35,55]
+xlim_graph_meanweight = [-10,100]
+
+
+run_lp_cs_1thetac_interactedmoments
+run_lp_cs_3thetacs_interactedmoments
+run_lp_cs_9thetacs_interactedmoments
