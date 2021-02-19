f8cded30b635b3963005ceb25cf10bb8a8dff1df
jonathanroth_MSFt <jonathanroth@g.harvard.edu>
2021-02-18 17:15:44 -0800

    Fixed yet another typo in implementation of new cox & shi code

--- Simulate_Data/lp_compute_confidence_sets.m
+++ Simulate_Data/lp_compute_confidence_sets.m
@@ -170,7 +170,7 @@ ds
         %[T_CC, c_RCC, c_CC] = rcc_test_fn(sqrt(nummarkets)^(-1) * y_T_tilde, eye(size(y_T_tilde,1)), X_T_tilde, zeros(size(y_T_tilde,1),1) , Sigma, nummarkets, 0, alpha);
         [T_CC,cv_CC, dof_n] = func_subCC(X_T_tilde, -y_T_tilde, Sigma, alpha);
         cc_rejection_vec(count,1) = (dof_n > 0) * (T_CC > cv_CC);
-        rcc_rejection_vec(count,1) = (T_CC > c_RCC) | ( (dof_n ==1) & (T_CC > chi2inv(1-2*alpha,dof_n)) ); %right now set RCC to 1 if dof_n == 1 and T_CC is above the 1-2*alpha cv
+        rcc_rejection_vec(count,1) = ((dof_n > 0) * (T_CC > cv_CC)) | ( (dof_n ==1) & (T_CC > chi2inv(1-2*alpha,dof_n)) ); %right now set RCC to 1 if dof_n == 1 and T_CC is above the 1-2*alpha cv
         
         
         count = count +1;
