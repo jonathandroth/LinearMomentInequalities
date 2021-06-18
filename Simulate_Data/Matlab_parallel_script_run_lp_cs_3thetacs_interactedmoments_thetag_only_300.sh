737a0c7cc99521192f287a8a1acef74e82ac1406
jonathanroth_MSFt <jonathanroth@g.harvard.edu>
2021-06-17 17:34:21 -0700

    Added back sh files for batch

---
+++ Simulate_Data/Matlab_parallel_script_run_lp_cs_3thetacs_interactedmoments_thetag_only_300.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH -o ../../Logs/run_lp_cs_3thetacs_interactedmoments_thetag_only.out
+#SBATCH -e ../../Logs/run_lp_cs_3thetacs_interactedmoments_thetag_only.err
+#SBATCH -p shared
+#SBATCH -c 36 
+#SBATCH -N 1 
+#SBATCH --mem 80000 
+#SBATCH -t 7-00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jonathanroth@g.harvard.edu
+
+source new-modules.sh
+module load matlab
+srun -n 1 -c 36 matlab -nodisplay -nosplash <run_lp_cs_3thetacs_interactedmoments_theta_g_only_300.m
