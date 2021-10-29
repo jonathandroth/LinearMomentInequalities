c9bb9d4448af9b0250cc275e139c133c3467925d
Jonathan Roth <jonathandroth@g.harvard.edu>
2021-10-29 12:31:33 -0400

    Saving cvxgen on server

---
+++ Simulate_Data/KMSCode/KMSportable_V3/CVXGEN/cvxgen/csolve.m
@@ -0,0 +1,34 @@
+% csolve  Solves a custom quadratic program very rapidly.
+%
+% [vars, status] = csolve(params, settings)
+%
+% solves the convex optimization problem
+%
+%   minimize(0)
+%   subject to
+%     A*x <= b
+%
+% with variables
+%        x  10 x 1
+%
+% and parameters
+%        A 132 x 10
+%        b 132 x 1
+%
+% Note:
+%   - Check status.converged, which will be 1 if optimization succeeded.
+%   - You don't have to specify settings if you don't want to.
+%   - To hide output, use settings.verbose = 0.
+%   - To change iterations, use settings.max_iters = 20.
+%   - You may wish to compare with cvxsolve to check the solver is correct.
+%
+% Specify params.A, ..., params.b, then run
+%   [vars, status] = csolve(params, settings)
+% Produced by CVXGEN, 2021-03-10 15:18:29 -0500.
+% CVXGEN is Copyright (C) 2006-2017 Jacob Mattingley, jem@cvxgen.com.
+% The code in this file is Copyright (C) 2006-2017 Jacob Mattingley.
+% CVXGEN, or solvers produced by CVXGEN, cannot be used for commercial
+% applications without prior written permission from Jacob Mattingley.
+
+% Filename: csolve.m.
+% Description: Help file for the Matlab solver interface.
