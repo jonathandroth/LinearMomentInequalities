8d9eb5ebf67160ca7b99e0e7fea6d97864cccee6
Jonathan Roth <jonathan_roth@brown.edu>
2021-11-04 10:43:19 -0400

    Cleaned out more files

--- cellReduce.m
+++
@@ -1,9 +0,0 @@
-function result = cellReduce( c, fun)
-
-numDimensions = length(size(c{1}));
-
-concatenatedCell = cat(numDimensions +1, c{:} );
-
-result = fun(concatenatedCell, numDimensions + 1);
-
-end
--- pi_array_fn.m
+++
@@ -1,8 +0,0 @@
-%Returns the pi_array given pi_star, the variance of the idiosyncratic
-%shocks, and the shocks
-function Pi_array = pi_array_fn( Pi_star_array, Zetaj_shocks_array, Zetajft_shocks_array, sigma_zetaj, sigma_zetajft)
-
-
-Pi_array = Pi_star_array + sigma_zetaj * Zetaj_shocks_array + sigma_zetajft * Zetajft_shocks_array;
-
-end
