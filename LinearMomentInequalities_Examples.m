b6d15a623f3420223c1fe6e3a593dde18b1eaf6b
Jonathan Roth <jonathan_roth@brown.edu>
2021-11-04 11:15:26 -0400

    Have updated examples

--- LinearMomentInequalities_Examples.m
+++ LinearMomentInequalities_Examples.m
@@ -6,7 +6,7 @@ Sigma = eye(length(y));
 Z_draws = randn(1000, length(y))';
 alpha = 0.05; 
 
-c_lf_lp(X,Z_draws, Sigma, alpha)
+c_lf = c_lf_lp(X,Z_draws, Sigma, alpha);
 
 %XX add something about studentiziation here
 
@@ -18,5 +18,8 @@ lp_hybrid_test_fn(y,X,Sigma,alpha, alpha/10)
 %%If you're using both LF / Hybrid at the same time, can use draws in
 %%hybrid XX 
 
-
 % If you want a CI for a component of delta, you can use this function XX
+cs_linear_delta_lp_fn(y,X,1,c_lf) 
+
+
+%If you want hybrid for a linear parameter, then can pass draws
